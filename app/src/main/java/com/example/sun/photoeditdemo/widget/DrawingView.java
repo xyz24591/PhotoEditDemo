291c475cd117cd762dad845fddcfe6103e6cfb45
2067601664 <2067601664@qq.com>
2018-01-11 11:11:59 +0800

    优化

--- app/src/main/java/com/example/sun/photoeditdemo/widget/DrawingView.java
+++ app/src/main/java/com/example/sun/photoeditdemo/widget/DrawingView.java
@@ -15,9 +15,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.LinkedList;
 
 /**
